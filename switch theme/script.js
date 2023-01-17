b07509095e33fe05c6e3abe1d2cb331ca99b2068
FabioFarinella96 <121029029+FabioFarinella96@users.noreply.github.com>
2023-01-17 18:46:45 +0100

    Add files via upload

---
+++ switch theme/script.js
@@ -0,0 +1,14 @@
+let switchTheme = document.querySelector(".switchButton");
+
+switchTheme.addEventListener("click", function () {
+  document.body.classList.toggle("light-theme");
+  document.body.classList.toggle("dark-theme");
+
+  const className = document.body.className;
+
+  if (className == "light-theme") {
+    this.textContent = "Dark";
+  } else {
+    this.textContent = "Light";
+  }
+});
